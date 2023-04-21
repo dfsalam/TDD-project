5a9f21a492aa42f985c87a7e72e387f19f7e3d2d
dfsalam <dfsalam@gmail.com>
2023-04-20 23:10:05 -0500

    Create test for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,6 +2,11 @@ require 'rspec'
 require './solver.rb'
 
 describe Solver do
-
+  describe '#factorial' do
+    it 'returns factorial of a given number' do
+      solver = Solver.new
+      expect(solver.factorial(5)).to eql(120)
+    end
+  end
 end
 
