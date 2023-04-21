782e47f67f557ab13c8ff3e16d531f8f6a48b1f6
Uthmanbello <uthmanbello88@gmail.com>
2023-04-21 15:33:30 +0100

    Add tests for fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -13,5 +13,23 @@ describe Solver do
       expect(subject.reverse("hello")).to eql("olleh")
     end
   end
+
+  describe '#fizzbuzz' do
+    it 'returns "fizz" for multiples of 3' do      
+      expect(subject.fizzbuzz(6)).to eql("fizz")
+    end
+
+    it 'returns "buzz" for multiples of 5' do
+      expect(subject.fizzbuzz(10)).to eql("buzz")
+    end
+
+    it 'returns "fizzbuzz" for multiples of both 3 and 5' do
+      expect(subject.fizzbuzz(15)).to eql("fizzbuzz")
+    end
+
+    it 'returns the integer if the integer is neither a multiple of 3 nor 5' do 
+      expect(subject.fizzbuzz(7)).to eql(7)
+    end
+  end
 end
 
