f5ae826e3b03a34b849cf12e66eea5ab69cd94a6
dfsalam <dfsalam@gmail.com>
2023-04-20 23:19:31 -0500

    Create test for reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,9 +3,14 @@ require './solver.rb'
 
 describe Solver do
   describe '#factorial' do
-    it 'returns factorial of a given number' do
-      solver = Solver.new
-      expect(solver.factorial(5)).to eql(120)
+    it 'returns factorial of a given number' do      
+      expect(subject.factorial(5)).to eql(120)
+    end
+  end
+
+  describe '#reverse' do
+    it 'takes one argument, a string word, and returns word reversed' do      
+      expect(subject.reverse("hello")).to eql("olleh")
     end
   end
 end
